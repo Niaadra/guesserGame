ab64d393eb1eb4c2993907a0ebe36d977f4cc8ee
Oscar <oscjon2001@skola.goteborg.se>
2020-10-19 12:19:37 +0200

    added a script that run the java program

---
+++ run_game.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+NAME="Oscar_Jonasson"
+MAP="Oscar_Jonasson_labb"
+
+echo -n "$NAME"
+echo "s program"
+
+mkdir $MAP 
+
+cp *.java $MAP
+
+cd $MAP
+
+echo -n "Running game from: "
+pwd
+
+echo "Compiling..."
+javac GuessingGame.java
+
+echo "Running game..."
+java GuessingGame
+
+echo "Done"
+
+echo "Removing class files..."
+rm *.class
+
+ls
+
+
